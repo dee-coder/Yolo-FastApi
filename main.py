cb0c30ffc45d9a9fee70ed7ea8222dc695417358
Upendra Singh <upendrarbt@gmail.com>
2022-12-24 11:41:58 +0000

    Update

--- main.py
+++ main.py
@@ -34,26 +34,44 @@ app.add_middleware(
 async def api_start():
     return {"Result": "Please add '/docs' after url and hit"}
 
-@app.post("/object-to-json")
+@app.post("/detect-object")
 async def detect_food_return_json_result(file: bytes = File(...)):
     input_image = get_image_from_bytes(file)
     results = model(input_image)
     detect_res = results.pandas().xyxy[0].to_json(orient="records")  # JSON img1 predictions
     detect_res = json.loads(detect_res)
-    return {"result": detect_res}
-
-
-@app.post("/object-to-img")
-async def detect_food_return_base64_img(file: bytes = File(...)):
-    input_image = get_image_from_bytes(file)
-    print("input_image =>", input_image)
-    results = model(input_image)
-    print("results =>", results)
-    a = results.render()  # updates results.imgs with boxes and labels
-    print("a =>", a)
-    for img in a:
+    b = results.render()
+    print("b =>", b)
+    for img in b:
         print('img', img)
         bytes_io = io.BytesIO()
+        print("bytes_io =>", bytes_io)
         img_base64 = Image.fromarray(img)
+        print("img_base64 =>", img_base64)
         img_base64.save(bytes_io, format="jpeg")
-    return Response(content=bytes_io.getvalue(), media_type="image/jpeg")
+        img_data = Response(content=bytes_io.getvalue(), media_type="image/jpeg")
+        print("img_data",img_data)
+    # return {
+    #     "result": detect_res
+    # }
+
+    return img_data
+
+
+@app.post("/object-to-json")
+async def detect_food_return_base64_img():
+    # input_image = get_image_from_bytes(file)
+    # print("input_image =>", input_image)
+    # results = model(input_image)
+    # print("results =>", results)
+    # a = results.render()  # updates results.imgs with boxes and labels
+    # print("a =>", a)
+    # for img in a:
+    #     print('img', img)
+    #     bytes_io = io.BytesIO()
+    #     img_base64 = Image.fromarray(img)
+    #     img_base64.save(bytes_io, format="jpeg")
+    image_value = detect_res
+    return {
+        "result": detect_res
+    }
